c164fc5dd554295772d6812bf5f8fb712f0d5542
Ham Otieno <hamotieno01@gmail.com>
2023-12-14 10:56:18 +0300

    complete 1

--- website.js
+++ website.js
@@ -21,3 +21,24 @@ function openmenu () {
 function closemenu () {
   sidemeu.style.right = '-200px'
 }
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyYSjt1NdkqAXPTsuQ-CoPftw8uojS13F0mJGvFJFy2olDV44QxrxxP6TR68HM5nnr1nQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = 'Message sent successfully'
+        setTimeout(function() {
+          msg.innerHTML = ''
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+
